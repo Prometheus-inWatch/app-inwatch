a29844bd359b8578b5dfeaddaa2c5b946d9823b5
Daniel Lupiañez Casares <lupidan@gmail.com>
2015-02-22 10:28:07 +0100

    Fixed adapter

--- app/src/main/java/com/prometheus/quitsmoking/adapter/QuestionaryAdapter.java
+++ app/src/main/java/com/prometheus/quitsmoking/adapter/QuestionaryAdapter.java
@@ -6,6 +6,12 @@ import android.support.v4.app.FragmentPagerAdapter;
 
 import com.prometheus.quitsmoking.fragment.QuestionCigarretesCostFragment;
 import com.prometheus.quitsmoking.fragment.QuestionCigarretesPerDayFragment;
+import com.prometheus.quitsmoking.fragment.QuestionFagerstrom1;
+import com.prometheus.quitsmoking.fragment.QuestionFagerstrom2;
+import com.prometheus.quitsmoking.fragment.QuestionFagerstrom3;
+import com.prometheus.quitsmoking.fragment.QuestionFagerstrom4;
+import com.prometheus.quitsmoking.fragment.QuestionFagerstrom5;
+import com.prometheus.quitsmoking.fragment.QuestionFagerstrom6;
 import com.prometheus.quitsmoking.fragment.UserDataFragment;
 import com.prometheus.quitsmoking.fragment.WelcomeFragment;
 
@@ -13,7 +19,7 @@ import com.prometheus.quitsmoking.fragment.WelcomeFragment;
  * Created by david on 20/2/15.
  */
 public class QuestionaryAdapter extends FragmentPagerAdapter {
-    private static int NUM_ITEMS = 4;
+    private static int NUM_ITEMS = 8;
 
     public QuestionaryAdapter(FragmentManager fragmentManager) {
         super(fragmentManager);
@@ -34,10 +40,18 @@ public class QuestionaryAdapter extends FragmentPagerAdapter {
             case 0:
                 return WelcomeFragment.newInstance();
             case 1:
-                return QuestionCigarretesPerDayFragment.newInstance();
+                return QuestionFagerstrom1.newInstance();
             case 2:
-                return QuestionCigarretesCostFragment.newInstance();
+                return QuestionFagerstrom2.newInstance();
             case 3:
+                return QuestionFagerstrom3.newInstance();
+            case 4:
+                return QuestionFagerstrom4.newInstance();
+            case 5:
+                return QuestionFagerstrom5.newInstance();
+            case 6:
+                return QuestionFagerstrom6.newInstance();
+            case 7:
                 return UserDataFragment.newInstance();
 
 
